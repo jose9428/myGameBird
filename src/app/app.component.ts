293d24d8c1956067a0e09ff8532975be41f7a471
Jose Luis Castro <76067475+jose9428@users.noreply.github.com>
2023-04-12 10:29:28 -0500

    Integracion del conteo del tiempo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-   cantCasas = [1,2,3,4,5,6,7,8,9];
+export class AppComponent implements OnInit{
+  cantCasas = [1,2,3,4,5,6,7,8,9];
+  puntajeRosa:number = 0;
+  puntajeAzul:number = 0;
+  tiempo:number = 0;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.fnInicializar();
+    this.iniciarTemporizador();
+  }
+
+  fnInicializar(){
+    this.puntajeRosa = 0;
+    this.puntajeAzul = 0;
+    this.tiempo = 60;
+  }
+
+  fnDisminuirTiempo(){
+    this.tiempo--;
+  }
+
+  iniciarTemporizador(){
+    let t = window.setInterval(() => {
+      if(this.tiempo <= 0){
+        this.fnDisminuirTiempo();
+        clearInterval(t);
+      }else{
+        this.fnDisminuirTiempo();
+      }
+    },1000)
+  }
 }
