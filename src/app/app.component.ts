8595dbe1da031c142a3b9a2545a66abc54e320f0
Jose Luis Castro <76067475+jose9428@users.noreply.github.com>
2023-04-12 11:01:17 -0500

    Implementacion de logica para la aparicion de aves

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Ave } from './models/Ave';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit{
-  cantCasas = [1,2,3,4,5,6,7,8,9];
+  aves : Ave [] = [];
   puntajeRosa:number = 0;
   puntajeAzul:number = 0;
   tiempo:number = 0;
+  maxAves:number = 9;
+  anteriorAzar: number = 0;
+  finalizoJuego:Boolean = false;
 
   constructor() { }
 
@@ -17,23 +21,69 @@ export class AppComponent implements OnInit{
     this.iniciarTemporizador();
   }
 
+  fnCrearAves(){
+    this.aves = [];
+    for(let i = 0; i< this.maxAves; i++){
+      const obj = new Ave();
+      obj.numero = i + 1;
+      obj.imagen = obj.numero % 2 == 0 ? 'aveRosa.png': 'aveAzul.png';
+      obj.visibilidad = false;
+      this.aves.push(obj);
+    }
+  }
+
+  fnGenerarAve(){
+    let azar = 0;
+
+    do{
+      azar = this.fnGenerarAleatoreo(1 , this.maxAves);
+    }while(azar == this.anteriorAzar);
+
+    this.anteriorAzar = azar;
+
+    for(let i = 0; i < this.aves.length; i++){
+      if(this.aves[i].numero == this.anteriorAzar){
+        this.aves[i].visibilidad = true;
+        break;
+      }
+    }
+  }
+
+  fnLimpiarAves(){
+    for(let i = 0; i < this.aves.length; i++){
+      this.aves[i].visibilidad = false;
+    }
+  }
+
+  fnGenerarAleatoreo(min: number,max : number){
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
   fnInicializar(){
     this.puntajeRosa = 0;
     this.puntajeAzul = 0;
     this.tiempo = 60;
+    this.finalizoJuego = true;
+    this.anteriorAzar = 0;
+    this.fnCrearAves();
   }
 
   fnDisminuirTiempo(){
     this.tiempo--;
+
+    if(this.tiempo < 0){
+      this.finalizoJuego = false;
+    }
   }
 
   iniciarTemporizador(){
     let t = window.setInterval(() => {
       if(this.tiempo <= 0){
-        this.fnDisminuirTiempo();
         clearInterval(t);
       }else{
         this.fnDisminuirTiempo();
+        this.fnLimpiarAves();
+        this.fnGenerarAve();
       }
     },1000)
   }
---
+++ src/app/models/Ave.ts
@@ -0,0 +1,11 @@
+export class Ave{
+  public numero?: number;
+  public imagen?: string;
+  public visibilidad?: Boolean;
+
+  constructor(){
+    this.numero = 0;
+    this.imagen = '';
+    this.visibilidad = false;
+  }
+}
